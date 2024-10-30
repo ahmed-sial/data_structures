9ac2efdd9c9b0c250603b3cfc76995288444a809
Ahmed Hassan <158329656+ahmed-sial@users.noreply.github.com>
2024-10-30 17:37:06 +0500

    Update FibonacciSeries.java

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -1,5 +1,7 @@
 /**
- * 
+ * Given a positive integer n, find the nth Fibonacci number.
+ * The Fibonacci sequence is a sequence where the next term is the sum of the previous two terms. The first two terms of the Fibonacci sequence are 0 followed by 1. The Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21
+ * F(0) = 0, F(1) = 1
  */
 
 import java.util.Scanner;
