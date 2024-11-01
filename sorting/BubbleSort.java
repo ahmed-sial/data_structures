20714a9449cc199fde69ca2f5e4013faa1b7256d
Ahmed Hassan <158329656+ahmed-sial@users.noreply.github.com>
2024-11-01 22:54:55 +0500

    Update BubbleSort.java

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -24,17 +24,17 @@ public class BubbleSort {
 		scanner.close();
 	}
 	public static void bubbleSort(int[] array) {
-		for (int i = 0; i < array.length - 1; i++) {// controls the total iteration that will be n - 1 (n = length of array) 
-			boolean swapped = false;// used to check if any swap is made. If swap is made then the array is still to be sorted else the array is sorted
-			for (int j = 0; j < array.length - i - 1; j++) { // checks from start upto last element of unsorted array. The sorted element moves to end of array
-				if (array[j] > array[j + 1]) { // if array[j] is greater then swap until it reaches it's correct position
+		for (int i = 0; i < array.length - 1; i++) {			// controls the total iteration that will be n - 1 (n = length of array) 
+			boolean swapped = false;				// used to check if any swap is made. If swap is made then the array is still to be sorted else the array is sorted
+			for (int j = 0; j < array.length - i - 1; j++) { 	// checks from start upto last element of unsorted array. The sorted element moves to end of array
+				if (array[j] > array[j + 1]) { 			// if array[j] is greater then swap until it reaches it's correct position
 					int temp = array[j];
 					array[j] = array[j + 1];
 					array[j + 1] = temp;
-					swapped = true;// if swap made then there is chance than array is still unsorted.
+					swapped = true;				// if swap made then there is chance than array is still unsorted.
 				}
 			}
-			if (swapped == false) // if no swaps then array is sorted to break from the outer loop.
+			if (swapped == false)					 // if no swaps then array is sorted to break from the outer loop.
 				break;
 		}
 	}
